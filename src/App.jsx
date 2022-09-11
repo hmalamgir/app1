04328b339ad55d3bdcf47b74d03ac5db35779db5
hmalamgir <hmalamgir002@gmail.com>
2022-09-11 15:30:50 +0600

    fisesdds

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import PostList from "./components/PostList";
-import PostState from "./context/post/PostState";
 import UserProvider from "./context/user/UserProvider";
 
 function App() {
   return (
-    <PostState>
-      <UserProvider>
-        <div className="container">
-          <h1>Shafin's & Rafin's App</h1>
-          <PostList />
-        </div>
-      </UserProvider>
-    </PostState>
+    <UserProvider>
+      <div className="container">
+        <h1>Shafin's & Rafin's App</h1>
+        <PostList />
+      </div>
+    </UserProvider>
   );
 }
 
